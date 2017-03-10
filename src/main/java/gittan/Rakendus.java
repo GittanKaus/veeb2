18a32c19cb993e2948f5751f5ce0d34a77b322cc
GittanKaus <GittanKaus@users.noreply.github.com>
2017-03-10 13:33:30 +0200

    algus2

---
+++ src/main/java/gittan/Rakendus.java
@@ -0,0 +1,38 @@
+package gittan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping; 
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@SpringBootApplication
+public class Rakendus {
+	@RequestMapping("/algus")
+    String tervitusfunktsioon() {
+        return "Hola!";
+    }
+	
+	
+	@RequestMapping("/tervita")
+	String tervitusfunktsioon3(String eesmini){
+		return "Hola, "+eesmini;
+	}
+	//Lisage teine tervitus funktsioon
+
+	@RequestMapping("/algus2")
+    String tervitusfunktsioon2() {
+        return "Hola amigo!";
+	}
+	
+    public static void main(String[] args) {
+		System.getProperties().put("server.port", 4217);
+        SpringApplication.run(Rakendus.class, args);
+    }
+	
+	
+	
+}
+
+//scl enable rh-maven33 bash
+//mvn package
+//java -jar target/boot3-1.0-SNAPSHOT.jar
